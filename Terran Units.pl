775846082dd9fff08ade061ea61bd17321b1cd53
Jin Min Lee <vfvxz73@naver.com>
2017-10-21 00:07:18 -0700

    Terran units added.

---
+++ Terran Units.pl
@@ -0,0 +1,97 @@
+prop(scv, mineral, 50).
+prop(scv, gas, 0).
+prop(scv, armour, 0).
+prop(scv, hp, 45).
+prop(scv, shield, 0).
+prop(scv, race, terran).
+prop(scv, attributeModifier, [light,biological,mechanical]).
+prop(scv, groundAttack, 5).
+prop(scv, bonusAttack, 0).
+prop(scv, bonusType, []).
+prop(scv, coolDown, 1.07).
+prop(scv, range, 0).
+prop(scv, speed, 3.94).
+
+prop(marine, mineral, 50).
+prop(marine, gas, 0).
+prop(marine, armour, 0).
+prop(marine, hp, 45).
+prop(marine, shield, 0).
+prop(marine, race, terran).
+prop(marine, attributeModifier, [light,biological]).
+prop(marine, groundAttack, 6).
+prop(marine, bonusAttack, 0).
+prop(marine, bonusType, []).
+prop(marine, coolDown, 0.61).
+prop(marine, range, 5).
+prop(marine, speed, 3.15).
+
+prop(marauder, mineral, 100).
+prop(marauder, gas, 25).
+prop(marauder, armour, 1).
+prop(marauder, hp, 125).
+prop(marauder, shield, 0).
+prop(marauder, race, terran).
+prop(marauder, attributeModifier, [armoured, biological]).
+prop(marauder, groundAttack, 10).
+prop(marauder, bonusAttack, 5).
+prop(marauder, bonusType, [armoured]).
+prop(marauder, coolDown, 1.07).
+prop(marauder, range, 6).
+prop(marauder, speed, 3.15).
+
+prop(reaper, mineral, 50).
+prop(reaper, gas, 50).
+prop(reaper, armour, 0).
+prop(reaper, hp, 60).
+prop(reaper, shield, 0).
+prop(reaper, race, terran).
+prop(reaper, attributeModifier, [light,biological]).
+prop(reaper, groundAttack, 8).
+prop(reaper, bonusAttack, 0).
+prop(reaper, bonusType, []).
+prop(reaper, coolDown, 0.79).
+prop(reaper, range, 5).
+prop(reaper, speed, 5.25).
+
+prop(ghost, mineral, 200).
+prop(ghost, gas, 100).
+prop(ghost, armour, 0).
+prop(ghost, hp, 100).
+prop(ghost, shield, 0).
+prop(ghost, race, terran).
+prop(ghost, attributeModifier, [biological, psionic]).
+prop(ghost, groundAttack, 10).
+prop(ghost, bonusAttack, 10).
+prop(ghost, bonusType, [light]).
+prop(ghost, coolDown, 1.07).
+prop(ghost, range, 6).
+prop(ghost, speed, 3.94).
+
+prop(hellbat, mineral, 100).
+prop(hellbat, gas, 0).
+prop(hellbat, armour, 0).
+prop(hellbat, hp, 135).
+prop(hellbat, shield, 0).
+prop(hellbat, race, terran).
+prop(hellbat, attributeModifier, [light, biological, mechanical]).
+prop(hellbat, groundAttack, 18).
+prop(hellbat, bonusAttack, 12).
+prop(hellbat, bonusType, [light]).
+prop(hellbat, coolDown, 1.43).
+prop(hellbat, range, 2).
+prop(hellbat, speed, 3.15).
+
+prop(cyclone, mineral, 150).
+prop(cyclone, gas, 100).
+prop(cyclone, armour, 1).
+prop(cyclone, hp, 180).
+prop(cyclone, shield, 0).
+prop(cyclone, race, terran).
+prop(cyclone, attributeModifier, [armoured, mechanical]).
+prop(cyclone, groundAttack, 3).
+prop(cyclone, bonusAttack, 2).
+prop(cyclone, bonusType, [armoured]).
+prop(cyclone, coolDown, 0.1).
+prop(cyclone, range, 4).
+prop(cyclone, speed, 4.13).
